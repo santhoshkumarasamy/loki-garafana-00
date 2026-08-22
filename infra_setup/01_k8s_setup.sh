4bf631ab2094e61205af6b0435c83ee68689753c
Santhosh Kumar <43289678+santhoshkumarasamy@users.noreply.github.com>
2026-08-22 07:19:16 +0530

    loki

--- infra_setup/01_k8s_setup.sh
+++ infra_setup/01_k8s_setup.sh
@@ -41,6 +41,8 @@ kubectl version --client
 
 minikube start --nodes=2
 
+minikube addons enable storage-provisioner-rancher
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
 ./get_helm.sh
